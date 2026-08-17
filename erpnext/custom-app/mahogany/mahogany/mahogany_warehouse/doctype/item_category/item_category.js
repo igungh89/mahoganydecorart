243af194aa1aee6c4c502b0d32dd90752e4d0593
Igun <119537210+igungh89@users.noreply.github.com>
2026-08-17 10:54:00 +0000

    Initial Mahogany project structure and ERPNext custom app

---
+++ erpnext/custom-app/mahogany/mahogany/mahogany_warehouse/doctype/item_category/item_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Igun Gunawan ST and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Item Category", {
+// 	refresh(frm) {
+
+// 	},
+// });
